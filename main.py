307c0e16b445ad06a1f62bc3e98fb042b901f4cd
vinsmazuka <distortion1988@mail.ru>
2022-03-06 23:03:44 +0900

    изм метод parse_contacts и добав ф main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from bs4 import BeautifulSoup, SoupStrainer
+from bs4 import BeautifulSoup
+import pandas
 import requests
 
 
@@ -15,8 +16,21 @@ class Parser:
         :param soup: class 'bs4.BeautifulSoup'
         :return: словарь с контактами организации
         """
-        telephones = list(map(lambda x: x.get_text(), list(soup.h1.find_all('a'))))
-        email, site = list(map(lambda x: x.get_text(), soup.find_all(target="_blank")))
+        telephones_list = list(map(lambda x: x.get_text(), list(soup.h1.find_all('a'))))
+        telephones = ",".join(telephones_list)
+        email_or_site = list(map(lambda x: x.get_text(), soup.find_all(target="_blank")))
+        if len(email_or_site) == 0:
+            email = ''
+            site = ''
+        elif len(email_or_site) == 2:
+            email, site = email_or_site
+        elif len(email_or_site) == 1 and '@' in email_or_site[0]:
+            email = email_or_site[0]
+            site = ''
+        else:
+            email = ''
+            site = email_or_site[0]
+
         contacts = {
             'telephones': telephones,
             'email': email,
@@ -25,29 +39,36 @@ class Parser:
         return contacts
 
 
-response = requests.get('https://excheck.pro/company/4401116480/contacts')
-new_soup = BeautifulSoup(response.text, 'html.parser')
+def main():
+    """
+    парсит файл эксель,
+    парсит сайт по инн организаций из файла
+    и добавляет в файл контактные данные организации
+    :return: none
+    """
+    organizations = pandas.read_excel('organizations.xlsx')
+    found_telephones = []
+    found_emails = []
+    found_sites = []
+    counter = 0
+    for inn in list(organizations['ИНН']):
+        response = requests.get(f'https://excheck.pro/company/{inn}/contacts')
+        new_soup = BeautifulSoup(response.text, 'html.parser')
+        result = Parser.parse_contacts(soup=new_soup)
+        found_telephones.append(result['telephones'])
+        found_emails.append(result['email'])
+        found_sites.append(result['site'])
+        counter += 1
+        print(counter)
+    organizations['telephone'] = found_telephones
+    organizations['email'] = found_emails
+    organizations['site'] = found_sites
+    organizations.to_excel('organizations.xlsx')
 
-result = Parser.parse_contacts(soup=new_soup)
-print(result)
 
-
-
-
-
-
-# try:
-#     email = soup.find(itemprop="email").get_text()
-# except AttributeError:
-#     pass
-# else:
-#     print(email)
-# try:
-#     site = soup.find(itemprop="url").get_text()
-# except AttributeError:
-#     pass
-# else:
-#     print(site)
+if __name__ == "__main__":
+    main()
+# response = requests.get('https://www.find-org.com/cli/1376165_ooo_ic_sapr')
 
 
 
